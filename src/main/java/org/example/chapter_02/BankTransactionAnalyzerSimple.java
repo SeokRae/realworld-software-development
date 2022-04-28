d3ad38e77487a8499bde58387ef4d6c237c12dd6
seokrae <kslbsh@gmail.com>
2022-04-28 23:33:42 +0900

    feat: comment description

--- src/main/java/org/example/chapter_02/BankTransactionAnalyzerSimple.java
+++ src/main/java/org/example/chapter_02/BankTransactionAnalyzerSimple.java
@@ -10,6 +10,12 @@ public class BankTransactionAnalyzerSimple {
 
 	private static final String RESOURCES = "src/main/resources/";
 
+	/**
+	 * 현재 시스템의 문제점
+	 * 1. 파일이 비어있는 경우
+	 * 2. 데이터에 문제가 있어서 금액을 파싱하지 못하는 경우
+	 * 3. 행의 데이터가 완전하지 않은 경우
+	 */
 	public static void main(final String[] args) throws IOException {
 		// Path 클래스는 파일 시스템의 경로를 가리킨다.
 		final Path path = Paths.get(RESOURCES + "bank-data-simple.csv");
